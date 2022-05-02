5e62f216a0cbcf787f2e73247d3d6de6e172d3c1
Minkus <minkus962@gmail.com>
2022-05-02 14:39:45 -0400

    templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 # Configuration
@@ -10,7 +10,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Welcome to the OSU CS 340 - Flask Tutorial!"
+    return render_template("main.j2", people=people_from_app_py)
+
+people_from_app_py = [
+{
+    "name": "Thomas",
+    "age": 33,
+    "location": "New Mexico",
+    "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
+}]
+
 
 
 # Listener
@@ -20,4 +47,4 @@ if __name__ == "__main__":
     #                                 ^^^^
     #              You can replace this number with any valid port
 
-    app.run(port=port)
+    app.run(port=port, debug=True)
